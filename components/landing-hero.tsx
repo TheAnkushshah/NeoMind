8c5b1504627a739ea158e037cee2620219015c4b
Ankush Shah <ankushsahvlogs@gmail.com>
2025-01-23 19:13:16 +0530

    hey

--- components/landing-hero.tsx
+++ components/landing-hero.tsx
@@ -12,7 +12,7 @@ export const LandingHero = () => {
     <div className="text-white font-bold py-36 text-center space-y-5">
       <div className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl space-y-5 font-extrabold">
         <h1>The Super AI Tool</h1>
-        <div className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+        <div className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 pl-6">
           <TypewriterComponent
             options={{
               strings: [
